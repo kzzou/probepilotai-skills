5f79df4596ec9a90e7e4e8082fbf6d0cf735986d
kzzou <zoukaizhang@gmail.com>
2026-07-10 13:30:14 +0900

    chore: disable retired Issue rating aggregator

---
+++ tools/aggregate_ratings.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""Compatibility shim: GitHub Issue ratings were retired in favor of the anonymous API."""
+
+print("GitHub Issue ratings are retired; no registry changes were made.")
