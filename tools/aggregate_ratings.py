557a72ea6a797ba15a63c8327a7c9e88178b6af6
kzzou <zoukaizhang@gmail.com>
2026-07-10 19:59:33 +0900

    remove: retired GitHub Issue rating flow

--- tools/aggregate_ratings.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility shim: GitHub Issue ratings were retired in favor of the anonymous API."""
-
-print("GitHub Issue ratings are retired; no registry changes were made.")
